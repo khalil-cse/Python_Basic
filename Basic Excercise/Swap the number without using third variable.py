275eb8e16c45b54650e8ba0a183d0c228a4cffec
KHALILUR RAHMAN <89288313+khalil518@users.noreply.github.com>
2023-10-18 12:13:13 +0600

    Add files via upload

---
+++ Basic Excercise/Swap the number without using third variable.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print(a,b)  #if a=5,b=6 then
+a=a+b       # a=5+6=11
+b=a-b       # b=11-6=5
+a=a-b       # a=11-5=6
+print(a,b)
